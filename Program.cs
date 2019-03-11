e780102bc304ce2969cce85b64c3378cab895321
KartikPandit <39631776+KartikPandit@users.noreply.github.com>
2019-03-11 23:58:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+class joe { 
+	static int josephus(int n) 
+	{ 
+		
+		int pos = 0;
+    for (int i = 1; i <= n; i++)
+      pos = (pos + 2) % i;
+    return pos+1 ;
+	} 
+	static void Main() 
+	{  
+		Console.Write("Enter the no ");
+		int n= int.Parse(Console.ReadLine());
+		Console.Write("The safe place is " + josephus(n)); 
+	} 
+} 
